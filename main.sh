1676dfdc181983964f6590eace37a5cd8bd74e43
taniguchi-kyoichi <kyoichi_dev.9401@icloud.com>
2024-07-23 11:05:51 +0900

    データを変更

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 # 実行するPythonスクリプト
 python src/main.py \
   -t -cfg src/configs/config_reacgan.yaml \
-  -data data/HAM10000 \
+  -data data/HAM10000_resized \
   -save checkpoints -metrics is fid prdc \
   -ref "train" -mpc --post_resizer "friendly" \
   --eval_backbone "InceptionV3_tf"
